10f28ecb1f47c311c70ae184df209e61459470f0
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-24 13:12:41 -0400

    Update App.js

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -3,10 +3,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>This is a bookstore</h1>
+      <BookList/>
       
     </div>
   );
+}
+
+function BookList() {
+  return (
+    <section> 
+     <Book/>
+      
+    </section>
+  )
+}
+
+
+const Book = () => {
+  return <article>this is a car</article>
 }
 
 export default App;
