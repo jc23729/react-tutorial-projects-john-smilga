e6a2953e36b7a47ff27d5b0ea5e13ac431653ce1
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-24 13:21:26 -0400

    Update App.js

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,27 +1,34 @@
-import './App.css';
-
+import "./App.css";
+// showing nested components
 function App() {
   return (
     <div className="App">
       <h1>This is a bookstore</h1>
-      <BookList/>
-      
+      <BookList />
+      <Image />
     </div>
   );
 }
 
 function BookList() {
   return (
-    <section> 
-     <Book/>
-      
+    <section>
+      <Book />
     </section>
-  )
+  );
 }
-
 
 const Book = () => {
-  return <article>this is a car</article>
-}
+  return <article>this is a car</article>;
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
+      alt=""
+    />
+  );
+};
 
 export default App;
