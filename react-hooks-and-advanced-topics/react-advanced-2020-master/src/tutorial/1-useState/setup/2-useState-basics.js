4a35b5fc39548abb3390d2ff1b50f806b33e05c0
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-25 17:28:14 -0400

    Update 2-useState-basics.js

--- react-hooks-and-advanced-topics/react-advanced-2020-master/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react-hooks-and-advanced-topics/react-advanced-2020-master/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -2,12 +2,19 @@ import React, { useState } from "react";
 //usestate is a function, its a named import
 
 const UseStateBasics = () => {
-  const [text, setText] = useState('useState using text, setText');
-  return (
-    <React.Fragment><h1>{text}</h1></React.Fragment>
-     );
-     
+  const [text, setText] = useState("useState using text, setText. Press the Change Title to change the Title");
+  const handleClick = () => {
+    setText("Woop, woop, you changed my state");
+  };
 
-}
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        change title
+      </button>
+    </React.Fragment>
+  );
+};
 
 export default UseStateBasics;
