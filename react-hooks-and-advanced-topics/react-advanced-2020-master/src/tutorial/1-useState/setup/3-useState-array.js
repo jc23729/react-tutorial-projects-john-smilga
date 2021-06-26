c8aa8f7551e6e059de5c88be33b39de848fa1aa9
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-26 12:36:43 -0400

    update

--- react-hooks-and-advanced-topics/react-advanced-2020-master/src/tutorial/1-useState/setup/3-useState-array.js
+++ react-hooks-and-advanced-topics/react-advanced-2020-master/src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  // this is a diffrent pay to pass use state  React. or import it
+  const [people, setPeople] = React.useState(data);
+  return <>
+    {people.map((person) => {
+      const { id, name } = person;
+      return (
+        <div key={id} className='item'>
+          <h4>{name}</h4>
+        </div>
+      )
+    })}
+  
+  </>;
 };
 
 export default UseStateArray;
