c4b8be9c5f5caf57f4d63a26a2197aafa90dc71d
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-26 12:04:48 -0400

    Update 2-useState-basics.js

--- react-hooks-and-advanced-topics/react-advanced-2020-master/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react-hooks-and-advanced-topics/react-advanced-2020-master/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 //usestate is a function, its a named import
 
 const UseStateBasics = () => {
-  const [text, setText] = useState("useState using text, setText. Press the Change Title to change the Title");
+  const [text, setText] = useState("random title");
   
   const handleClick = () => {
     if (text === 'random title') {
       setText('Hello World');
+    } else {
+      setText('random title')
     }
   };
 
