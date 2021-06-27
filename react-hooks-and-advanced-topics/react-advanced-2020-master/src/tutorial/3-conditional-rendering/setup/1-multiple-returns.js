dd6973397bd3ee990ffe27c93bebdd4fb97a2bc3
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-27 15:08:04 -0400

    Update 1-multiple-returns.js

--- react-hooks-and-advanced-topics/react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ react-hooks-and-advanced-topics/react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,23 +1,40 @@
-import React, { useState, useEffect } from "react";
-const url = "https://api.github.com/users/QuincyLarson";
+import React, { useState, useEffect } from 'react';
+const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
-  const [user, setUser] = useState("default user");
+  const [user, setUser] = useState('default user');
 
   useEffect(() => {
     fetch(url)
-      .then(resp.json())
-      .then((user) => console.log(user))
-      .catch((error) => console.error(error));
+      .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 299) {
+          return resp.json();
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(resp.statusText);
+        }
+      })
+      .then((user) => {
+        const { login } = user;
+        setUser(login);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
   }, []);
+
   if (isLoading) {
-    return <h2>....loading</h2>;
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
   }
   if (isError) {
     return (
       <div>
-        <h1>isError...</h1>
+        <h1>Error....</h1>
       </div>
     );
   }
@@ -28,53 +45,5 @@ const MultipleReturns = () => {
   );
 };
 
-// import React, { useState, useEffect } from 'react';
-// const url = 'https://api.github.com/users/QuincyLarson';
-// const MultipleReturns = () => {
-//   const [isLoading, setIsLoading] = useState(true);
-//   const [isError, setIsError] = useState(false);
-//   const [user, setUser] = useState('default user');
-
-//   useEffect(() => {
-//     fetch(url)
-//       .then((resp) => {
-//         if (resp.status >= 200 && resp.status <= 299) {
-//           return resp.json();
-//         } else {
-//           setIsLoading(false);
-//           setIsError(true);
-//           throw new Error(resp.statusText);
-//         }
-//       })
-//       .then((user) => {
-//         const { login } = user;
-//         setUser(login);
-//         setIsLoading(false);
-//       })
-//       .catch((error) => console.log(error));
-//   }, []);
-
-//   if (isLoading) {
-//     return (
-//       <div>
-//         <h1>Loading...</h1>
-//       </div>
-//     );
-//   }
-//   if (isError) {
-//     return (
-//       <div>
-//         <h1>Error....</h1>
-//       </div>
-//     );
-//   }
-//   return (
-//     <div>
-//       <h1>{user}</h1>
-//     </div>
-//   );
-// };
-
-// export default MultipleReturns;
-
 export default MultipleReturns;
+
