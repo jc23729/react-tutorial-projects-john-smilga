fddf525bd57cf92bd3e4f6cdce0137f329724a39
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-27 14:54:01 -0400

    Update 1-multiple-returns.js

--- react-hooks-and-advanced-topics/react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ react-hooks-and-advanced-topics/react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,15 +1,30 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
-
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("default user");
 
 
-  if (loading) {
-   return <h2>....loading</h2>
- }
-  return <h2>not loading</h2>;
+  useEffect(() => {
+    fetch(url).then((resp.json)).catch(error => console.error(error));
+    
+  }, [])
+  if (isLoading) {
+    return <h2>....loading</h2>;
+  }
+  if (isError) {
+    return (
+      <div>
+        <h1>isError...</h1>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>{user}</h1>
+    </div>
+  );
 };
 
 // import React, { useState, useEffect } from 'react';
