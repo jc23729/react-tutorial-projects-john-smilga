4f6fc34435210b0695ddd7cfab38d8755b967dec
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-27 13:07:22 -0400

    Update 3-useEffect-fetch-data.js

--- react-hooks-and-advanced-topics/react-advanced-2020-master/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ react-hooks-and-advanced-topics/react-advanced-2020-master/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 const url = "https://api.github.com/users";
-
+//second argument
 const UseEffectFetchData = () => {
   const [users, setUsers] = useState([]);
 
@@ -14,14 +14,18 @@ const UseEffectFetchData = () => {
   };
   useEffect(() => {
     getUsers();
+    // if we don't setup an empty array, or depencency array it will be an infinite loop because the useEffect runs after each rerender
   }, []);
   return (
     <>
       <h3>github users</h3>
       <ul className="users">
+        {/* we iterate over all users for each and every users */}
         {users.map((user) => {
+          // user is an object so we desctruct the user 
           const { id, login, avatar_url, html_url } = user;
           return (
+            // remember each and every time we have a list we have to pass this unique key
             <li key={id}>
               <img src={avatar_url} alt={login} />
               <div>
