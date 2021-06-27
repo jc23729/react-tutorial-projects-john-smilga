0cdf1776c394e2b16afb874168f5ba7fb1ac05ec
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-27 12:01:44 -0400

    Update 2-useEffect-cleanup.js

--- react-hooks-and-advanced-topics/react-advanced-2020-master/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ react-hooks-and-advanced-topics/react-advanced-2020-master/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from 'react';
 
 const UseEffectCleanup = () => {
   const [size, setSize] = useState(window.innerWidth);
+  console.log(size);
   return <h2>useEffect cleanup</h2>;
 };
 
